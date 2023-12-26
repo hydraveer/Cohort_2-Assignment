bbee9ea41af154302c4ba8234a31cc77016590fa
Virendra Patel <85815897+hydraveer@users.noreply.github.com>
2023-12-27 00:03:01 +0530

    Delete cohort_solution directory

--- cohort_solution/week-2/easy/1-counter.js
+++
@@ -1,8 +0,0 @@
-let n=0
-function counter1(){
-   setInterval(()=>{
-    n=n+1
-    console.log(n);
-   },1000) 
-}
-counter1()
--- cohort_solution/week-2/easy/2-counter.js
+++
@@ -1,9 +0,0 @@
-let n=0
-function repeat(){
-  setTimeout(()=>{
-    n=n+1
-    console.log(n);
-    repeat()
-  },1000)
-}
-repeat()
